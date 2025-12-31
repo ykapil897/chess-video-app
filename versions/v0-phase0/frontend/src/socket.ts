9e07806705ab1aae25bb5b7008266aedd0b5d278
ykapil897 <ykapil897@gmail.com>
2025-12-31 11:07:23 +0530

    fro deployment

--- versions/v0-phase0/frontend/src/socket.ts
+++ versions/v0-phase0/frontend/src/socket.ts
@@ -1,2 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:3001");
+// export const socket = io("http://localhost:3001");
+export const socket = io(
+  import.meta.env.VITE_BACKEND_URL as string
+);
